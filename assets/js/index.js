d5225f143b403ff513790a319a0e8a178dcd02f0
Aldo Jara <aldoandre.jc@gmail.com>
2022-11-20 22:36:31 -0300

    Modificaciones finales, cambio de elemento Js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,35 @@
 $(function () {
-// Tooltip en botón formulario e iconos de footer//
-var tooltipTriggerList =
-    [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map
-    (function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl)
+    // Tooltip en botón formulario e iconos de footer//
+    var tooltipTriggerList =
+        [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map
+        (function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl)
+        })
+
+    //Popover en icono de Navbar//
+    const popoverTriggerList =
+        document.querySelectorAll('[data-bs-toggle="popover"]')
+    const popoverList = [...popoverTriggerList].map
+        (popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+    // Cambio de color de Navbar //
+    $(window).scroll(function () {
+        if ($("#menu").offset().top > 310) {
+            $("#menu").addClass("bg-dark");
+        } else {
+            $("#menu").removeClass("bg-dark");
+        }
+
+        // Toast en titulo sección destacados//
+        const toastTrigger = document.getElementById('textoDestacado')
+        const toastLiveExample = document.getElementById('liveToast')
+        if (toastTrigger) {
+            toastTrigger.addEventListener('click', () => {
+                const toast = new bootstrap.Toast(toastLiveExample)
+
+                toast.show()
+            })
+        }
     })
-
-//Popover en icono de Navbar//
-const popoverTriggerList =
-    document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map
-    (popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
-
-// Cambio de color de Navbar //
-$(window).scroll(function () {
-    if ($("#menu").offset().top > 310) {
-        $("#menu").addClass("bg-dark");
-    } else {
-        $("#menu").removeClass("bg-dark");
-    }
-
-    // Alerta en card de seccion destacados//
-    $(function () {
-        $('[class="text-destacado"]').h2()
-    })
-    $("#textoDestacado").click(function () {
-        alert("El correo fue enviado correctamente...");
-    });
-})
 });
